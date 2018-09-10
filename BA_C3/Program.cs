3822f69059de55807b112c3f5f6384b2064e2101
Domantas Birbalas <s028204@ad.viko.lt>
2018-09-10 23:54:12 +0300

    Add project files.

---
+++ BA_C3/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace BA_C3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Group> groupsList = new List<Group>();
+
+            using (var reader = new StreamReader(@"New Text Document (2).csv"))
+            {
+                bool passedFirstline = false;
+
+                while (!reader.EndOfStream)
+                {
+                    var values = reader.ReadLine().Split(';');
+
+                    if (passedFirstline == true)
+                    {
+
+
+                        var group = new Group
+                        {
+                            Id = int.Parse(values[0]),
+                            Name = values[1],
+                            Type_Id = int.Parse(values[2]),
+                            Type_Label = values[3],
+                            Lan_Id = int.Parse(values[4]),
+                            Lan_Label = values[5],
+                            Children = int.Parse(values[6]),
+                            Free_space = int.Parse(values[7])
+
+                        };
+                        groupsList.Add(group);
+
+                    }
+                    else
+                    {
+                        passedFirstline = true;
+                    }
+
+
+
+                }
+
+
+            }
+
+            List<Group> MinMax = new List<Group>();
+            var maxValue = MaxValue(groupsList);
+            var minValue = MinValue(groupsList);
+            
+
+
+            foreach (var item in groupsList)
+            {
+                if (item.Children == maxValue || item.Children == minValue)
+                {
+                    MinMax.Add(item);
+                }
+
+            }
+
+            
+            foreach (var item in MinMax)
+            {
+                string a = item.Name.Substring(0, 3);
+                string b = item.Lan_Label.Substring(0, 3);
+                string c = item.Type_Label.Remove(0, 4);
+               c= c.Replace("iki", "-");
+               c= c.Replace("metų", "");
+
+                Console.WriteLine("{0}_{1}_{2}", a , b , c);
+
+            }
+
+            List<string> languages = new List<string>();
+            foreach (var item in groupsList) 
+            {
+                if (!languages.Contains(item.Lan_Label))
+                {
+                    languages.Add(item.Lan_Label);
+                    Console.WriteLine(item.Lan_Label);
+
+                }
+
+            }
+
+
+
+
+            Console.Read();
+
+
+
+        }
+
+        public static int MaxValue(List<Group> list)
+        {
+            int max = int.MinValue;
+
+            foreach (var item in list)
+            {
+                if (item.Children > max)
+                {
+                    max = item.Children;
+                }
+
+               
+            }
+            Console.WriteLine(max);
+            return max;
+
+        }
+
+        public static int MinValue(List<Group> list)
+        {
+
+            int min = int.MaxValue;
+            foreach (var item in list)
+            {
+                if (item.Children < min)
+                {
+                    min = item.Children;
+                }
+
+               
+            }
+            Console.WriteLine(min);
+            return min;
+
+        }
+
+      
+    }
+
+}
---
+++ BA_C3/class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BA_C3
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Type_Id { get; set; }
+        public string Type_Label { get; set; }
+        public int Lan_Id { get; set; }
+        public string Lan_Label { get; set; }
+        public int Children { get; set; }
+        public int Free_space { get; set; }
+    }
+
+}
